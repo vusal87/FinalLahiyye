064cffbcf18232d136141bb938cdaaf14097ebd7
vusal87 <vusalhaciyev9@gmail.com>
2017-07-07 10:40:21 -0700

    Add files via upload

--- kensisys/js/main.js
+++ kensisys/js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
+
 $(function(){
 $(".slider .images li").hide();
 $(".slider .images li:first").show();
-$(".slider .btn1 button:first").addClass("active");
-$(".slider .btn1 button").click(function(){
+$(".slider-buttons .item:first").addClass("active");
+$(".slider-buttons .item").click(function(){
 	var indis=$(this).index();
-$(".slider .btn1 button").removeClass("active");
+$(".slider-buttons .item").removeClass("active");
 	$("this").addClass("active");
 	$(".slider .images li").hide();
 	$(".slider .images li:eq("+indis+")").fadeIn("slow");		
@@ -15,10 +16,32 @@ $(".slider .btn1 button").removeClass("active");
 
 $(".slider .images li p")
   .animate({height: "hide"}, 1000)
-  .text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. provident  placeat.")
+  // .text("<h2>A HUHE SELECTION</h2>")
   .animate({height: "show"}, 1000);
+
+
+var mycls = $(".slider-buttons");
+  mycls.find(".item").click(function() {
+  mycls.find(".item").removeClass("active");
+  $(this).addClass("active");
+
+});
+
+
 
 
 });
 
 
+
+
+
+
+
+
+// var mycls = $(".cls");
+
+// mycls.find(".item").click(function() {
+//   mycls.find(".item").removeClass("active");
+//   $(this).addClass("active");
+// });
