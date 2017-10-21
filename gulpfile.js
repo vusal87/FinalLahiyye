13629bcbc6738059496f99d3cf2654432b091e21
vusal87 <vusalhaciyev9@gmail.com>
2017-10-21 10:32:51 +0400

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass=require('gulp-sass');
+var rename=require('gulp-rename');
+var notify=require('gulp-notify');
+
+
+cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('./sass'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./app'))
+    .pipe(notify('donel'))
+});
+gulp.task('watch',function(){
+	gulp.watch('./sass/**/*.scss',['sass'])
+});
+
+gulp.task("default", ["sass", "watch"]);
